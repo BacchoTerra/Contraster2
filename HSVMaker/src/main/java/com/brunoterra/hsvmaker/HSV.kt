fd0a5b03c5c888c860c14ea030eecbd8179b092d
BacchoTerra <brunobacchot@hotmail.com>
2023-08-22 15:48:52 -0300

    Initial commit

---
+++ HSVMaker/src/main/java/com/brunoterra/hsvmaker/HSV.kt
@@ -0,0 +1,9 @@
+package com.brunoterra.hsvmaker
+
+import androidx.annotation.FloatRange
+
+data class HSV(
+    @FloatRange(from = 0.0, to = 360.0) val hue: Float,
+    @FloatRange(from = 0.0, to = 1.0) val saturation: Float,
+    @FloatRange(from = 0.0, to = 1.0) val value: Float,
+)
