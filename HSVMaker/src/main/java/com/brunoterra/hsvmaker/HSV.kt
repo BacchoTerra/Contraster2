0d8cbc2dde97112843c1e2bd058984acfb98610d
BacchoTerra <brunobacchot@hotmail.com>
2023-08-22 15:48:53 -0300

    Created basic HSV color conversion

--- HSVMaker/src/main/java/com/brunoterra/hsvmaker/HSV.kt
+++ HSVMaker/src/main/java/com/brunoterra/hsvmaker/HSV.kt
@@ -3,7 +3,7 @@ package com.brunoterra.hsvmaker
 import androidx.annotation.FloatRange
 
 data class HSV(
-    @FloatRange(from = 0.0, to = 360.0) val hue: Float,
-    @FloatRange(from = 0.0, to = 1.0) val saturation: Float,
-    @FloatRange(from = 0.0, to = 1.0) val value: Float,
+    @FloatRange(from = 0.0, to = 360.0) var hue: Float,
+    @FloatRange(from = 0.0, to = 1.0) var saturation: Float,
+    @FloatRange(from = 0.0, to = 1.0) var value: Float,
 )
--- HSVMaker/src/main/java/com/brunoterra/hsvmaker/HSVMaker.kt
+++ HSVMaker/src/main/java/com/brunoterra/hsvmaker/HSVMaker.kt
@@ -3,10 +3,10 @@ package com.brunoterra.hsvmaker
 import android.graphics.Color
 import androidx.core.graphics.ColorUtils
 
-class HSVMaker {
+object HSVMaker {
 
-    fun createFromHSV(hsv: HSV, alpha: Int): Int {
+    fun createFromHSV(hsv: HSV): Int {
         val rgb = ColorUtils.HSLToColor(floatArrayOf(hsv.hue, hsv.saturation, hsv.value))
-        return Color.argb(alpha, Color.red(rgb), Color.green(rgb), Color.blue(rgb))
+        return Color.rgb(Color.red(rgb), Color.green(rgb), Color.blue(rgb))
     }
 }
